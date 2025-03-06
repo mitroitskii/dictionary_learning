10b0bf02426e35d65117497057c939245488b21e
mitroitskii <troitskii.d@northeastern.edu>
2025-03-05 20:01:46 -0500

    Added load_dictionary from orig repo to utils.py

--- dictionary_learning/utils.py
+++ dictionary_learning/utils.py
@@ -6,6 +6,15 @@ import signal
 import time
 import sys
 
+from .trainers.top_k import AutoEncoderTopK
+from .trainers.batch_top_k import BatchTopKSAE
+from .dictionary import (
+    AutoEncoder,
+    GatedAutoEncoder,
+    AutoEncoderNew,
+    JumpReluAutoEncoder,
+    CrossCoder,
+)
 
 class ExitHandler:
     """
@@ -120,3 +129,33 @@ def zst_to_generator(data_path, field="text"):
             yield json.loads(line)[field]
 
     return generator()
+
+def load_dictionary(base_path: str, device: str) -> tuple:
+    ae_path = f"{base_path}/ae.pt"
+    config_path = f"{base_path}/config.json"
+
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    dict_class = config["trainer"]["dict_class"]
+
+    if dict_class == "AutoEncoder":
+        dictionary = AutoEncoder.from_pretrained(ae_path, device=device)
+    elif dict_class == "GatedAutoEncoder":
+        dictionary = GatedAutoEncoder.from_pretrained(ae_path, device=device)
+    elif dict_class == "AutoEncoderNew":
+        dictionary = AutoEncoderNew.from_pretrained(ae_path, device=device)
+    elif dict_class == "AutoEncoderTopK":
+        k = config["trainer"]["k"]
+        dictionary = AutoEncoderTopK.from_pretrained(ae_path, k=k, device=device)
+    elif dict_class == "BatchTopKSAE":
+        k = config["trainer"]["k"]
+        dictionary = BatchTopKSAE.from_pretrained(ae_path, k=k, device=device)
+    elif dict_class == "JumpReluAutoEncoder":
+        dictionary = JumpReluAutoEncoder.from_pretrained(ae_path, device=device)
+    elif dict_class == "CrossCoder":
+        dictionary = CrossCoder.from_pretrained(ae_path, device=device)
+    else:
+        raise ValueError(f"Dictionary class {dict_class} not supported")
+
+    return dictionary, config
